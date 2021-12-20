9abf5d79e42b4d179cee1a668da9e54f16c1cdfd
Rodrigo Engelberg <rodrigoengelberg@gmail.com>
2021-12-20 19:28:01 -0300

    Creating paggination on Table

--- src/app/modules/members/pages/MemberListPage.tsx
+++ src/app/modules/members/pages/MemberListPage.tsx
@@ -18,6 +18,20 @@ const MemberListPage: React.FC = () => {
   const [members, setMembers] = useState<MemberModel[]>([])
   const dispatch = useDispatch()
 
+  const calculateRange = (data: any, rowsPerPage: any) => {
+    const range = []
+    const num = Math.ceil(data.length / rowsPerPage)
+    let i = 1;
+    for (let i = 1; i <= num; i++) {
+      range.push(i)
+    }
+    return range
+  }
+
+  const sliceData = (data: any, page: any, rowsPerPage: any) => {
+    return data.slice((page - 1) * rowsPerPage, page * rowsPerPage)
+  }
+
   const exportToCsv = () => {
     let membersVote: MemberModel[]
 
